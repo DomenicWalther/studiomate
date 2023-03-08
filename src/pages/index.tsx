4c8bea27b72cfe8b078047b0fe3127f1f0b64e03
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2023-03-08 03:00:53 +0100

    Add Login via. Discord

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@ const Home: NextPage = () => {
     <div>
       <Masthead />
       <LandingPageInformation />
+      <AuthShowcase />
       <Footer />
     </div>
   );
@@ -30,12 +31,12 @@ const AuthShowcase: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
+      <p className="text-center text-2xl text-black">
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
         {secretMessage && <span> - {secretMessage}</span>}
       </p>
       <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        className="text-blacks rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
         onClick={sessionData ? () => void signOut() : () => void signIn()}
       >
         {sessionData ? "Sign out" : "Sign in"}
