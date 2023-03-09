3635af0db782c68e3ebf0744b07fd50271e7ae9c
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2023-03-09 23:28:11 +0100

    feat: Finishing Design of Navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
-import { type StaticImageData } from "next/image";
 import React from "react";
 import Searchbar from "./Searchbar";
 import Avatar from "./Users/Avatar";
+import Image from "next/image";
+import { IconContext } from "react-icons";
+
+import { IoNotificationsOutline, IoCalendarOutline } from "react-icons/io5";
+import { BiMessageDetail } from "react-icons/bi";
 
 interface userValues {
   userName: string;
@@ -9,19 +13,35 @@ interface userValues {
 }
 
 const Navbar: React.FC<userValues> = ({ userName, avatarPath }: userValues) => {
+  const iconSize = 25;
   return (
     <div className="flex h-20 items-center justify-between bg-white p-8 pr-10 font-sans lg:pr-44">
-      <div>Logo</div>
+      <div className="logo flex items-center gap-4 text-neutral-800">
+        <Image
+          src="/svg/studiomate_logo.svg"
+          alt="Logo"
+          width={50}
+          height={50}
+        />
+        <h3 className="text-base">STUDIOMATE</h3>
+      </div>
       <h2 className="text-3xl font-bold text-supporting-lblue-900">
         Good morning,{" "}
-        <span className="text-primary-purple-600">{userName}</span>
+        <span className="primar text-primary-purple-600 ">{userName}</span>
       </h2>
+
       <Searchbar />
       <div className="flex gap-5">
         <div className="flex items-center gap-5">
-          <p>Calendar</p>
-          <p>Messages</p>
-          <p>Notifications</p>
+          <IconContext.Provider
+            value={{
+              className: "fill-primary-purple-600 stroke-primary-purple-600",
+            }}
+          >
+            <IoCalendarOutline size={iconSize} />
+            <BiMessageDetail size={iconSize} />
+            <IoNotificationsOutline size={iconSize} />
+          </IconContext.Provider>
         </div>
         <Avatar avatarPath={avatarPath} />
       </div>
